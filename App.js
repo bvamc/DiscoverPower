2ad5cfc3a6008013769ad30ca8d7315536abe98f
Vamsidhar <58134679+bvamc@users.noreply.github.com>
2020-02-03 22:25:56 -0500

    Demo version

--- App.js
+++ App.js
@@ -2,13 +2,13 @@ import * as React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
-import { HomeScreen } from './screen/HomeScreen';
+// import { HomeScreen } from './screen/HomeScreen';
 import { PowerComparision } from './screen/PowerComparision';
 import {SignIn} from './screen/SignIn';
 
 const RootStack = createStackNavigator(
     {
-      Home: HomeScreen,
+      // Home: HomeScreen,
       Details: PowerComparision,
       SignIn: SignIn
     },
--- screen/SignIn.js
+++ screen/SignIn.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import {View, TextInput, Text, TouchableOpacity} from 'react-native';
 import { Button } from 'react-native-elements';
+import {faPlug} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 
 export class SignIn extends React.Component {
     constructor(props) {
@@ -27,12 +29,16 @@ export class SignIn extends React.Component {
         // Authentication
         if(this.state.username.length>0 && this.state.pass.length >0){
             console.log("Success");
-            this.props.navigation.navigate('Home');
+            this.props.navigation.navigate('Details');
         }
         // Move to different screen
     };
     render() {
         return (<View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+            <Text style={{flexDirection: 'row', justifyContent: 'center', width:'80%', fontStyle: 'italic', fontSize: 17,  marginTop: '20%', marginBottom: '20%'}}>
+                Energy earns or simply burns, choice is yours.{'\n'}
+                Energy misused cannot be excused.
+            </Text>
             <TextInput
                 style={{ alignItems:'center', justifyContent:'center', width: '75%', margin: 5,  borderColor: 'gray', borderWidth: 1 }}
                 onChangeText={text => this.handleUserNameChange(text)}
@@ -48,10 +54,13 @@ export class SignIn extends React.Component {
             />
             <Button style={{alignItems:'center', justifyContent:'center'}} title="Sign In" onPress={this.signIn}/>
 
-            <Text style={{flexDirection: 'row', justifyContent: 'center', width:'80%', fontStyle: 'italic', fontSize: 17,  marginTop: '40%'}}>
-                Energy earns or simply burns, choice is yours.{'\n'}
-                Energy misused cannot be excused.
-            </Text>
+
+            <TouchableOpacity  style={{paddingVertical: 15}}>
+                <Text style={{ paddingTop: '30%', fontSize: 14, color: '#2e78b7'}}>
+                    Check your power consumption <FontAwesomeIcon icon= {faPlug} />
+                </Text>
+            </TouchableOpacity>
+
         </View>);
     }
 }
